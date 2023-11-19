c5531586f53dd967581799eb87b1ca2f25966217
Vinicius O. das Neves Rodrigues <126347863+ViniciusONevesRodrigues@users.noreply.github.com>
2023-11-19 11:17:30 -0300

    Add ConfirmPassword, ShowPassword and Confirmdelete

---
+++ gamix-crud/gamix-crud/src/main/java/org/gamix/models/User.java
@@ -0,0 +1,76 @@
+package org.gamix.models;
+
+public class User {
+
+    private Integer id;
+    private String username;
+    private String email;
+    private String icon;
+    private String bg;
+    private PasswordUser passwordUser;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public PasswordUser getPasswordUser() {
+        return passwordUser;
+    }
+
+    public void setPasswordUser(PasswordUser passwordUser) {
+        this.passwordUser = passwordUser;
+    }
+
+    public String getBg() {
+    	return bg;
+    }
+    
+    public void setBg(String bg) {
+    	this.bg = bg;
+    }
+    
+    public void partialUserInput(User partialUserInput) {
+        if (partialUserInput != null) {
+            if (partialUserInput.getUsername() != null) {
+                this.username = partialUserInput.getUsername();
+            }
+            if (partialUserInput.getEmail() != null) {
+                this.email = partialUserInput.getEmail();
+            }
+            if (partialUserInput.getIcon() != null) {
+                this.icon = partialUserInput.getIcon();
+            }
+            if (partialUserInput.getBg() != null ) {
+            	this.bg = partialUserInput.getBg();
+            }
+        }
+    }
+}
